7e0bed72babdbb2f66ae2407baa7a13963c31bb2
Nick Chursin <nicktestqa@yahoo.com>
2019-01-10 15:57:49 +0200

    Added docs

--- 10MinutesMail.Net/TenMinuteMailNet.cs
+++ 10MinutesMail.Net/TenMinuteMailNet.cs
@@ -13,20 +13,32 @@ namespace _10MinuteMail.Net
         private const string BaseUrl = "https://10minutemail.net";
         private readonly HttpClient _httpClient;
 
+        /// <summary>Initializes a new instance of the <see cref="TenMinuteMailNet"/> class. https://10minutemail.net - client</summary>
         public TenMinuteMailNet()
         {
             _httpClient = new HttpClient();
         }
 
+        /// <summary>Gets the mail response.</summary>
+        /// <value>The mail response.</value>
         public MailResponse MailResponse => GetMailResponseAsync().Result;
 
+        /// <summary>Gets the email address.</summary>
+        /// <value>The email address.</value>
         public string EmailAddress => GetEmailAddressAsync().Result;
 
+        /// <summary>Gets the seconds left.</summary>
+        /// <value>The seconds left.</value>
         public long SecondsLeft => GetSecondsLeftAsync().Result;
 
+        /// <summary>Gets the email count.</summary>
+        /// <value>The email count.</value>
         public int EmailCount => GetEmailsCountAsync().Result;
         public MailContent LastEmail => GetLastEmailAsync().Result;
 
+        /// <summary>Gets the response string.</summary>
+        /// <param name="responseUrl">The response URL.</param>
+        /// <returns></returns>
         private async Task<string> GetResponseString(string responseUrl)
         {
             var httpRequest = new HttpRequestMessage(HttpMethod.Get, $"{BaseUrl}{responseUrl}");
@@ -37,6 +49,8 @@ namespace _10MinuteMail.Net
             return await responseMessage.Content.ReadAsStringAsync();
         }
 
+        /// <summary>Gets the mail response asynchronous.</summary>
+        /// <returns></returns>
         private async Task<MailResponse> GetMailResponseAsync()
         {
             var mailResponse = new MailResponse();
@@ -47,6 +61,9 @@ namespace _10MinuteMail.Net
             return mailResponse;
         }
 
+        /// <summary>Gets the mail content asynchronous.</summary>
+        /// <param name="mailId">The mail identifier.</param>
+        /// <returns></returns>
         public async Task<MailContent> GetMailContentAsync(string mailId)
         {
             var mailContent = new MailContent();
@@ -58,29 +75,40 @@ namespace _10MinuteMail.Net
             return mailContent;
         }
 
+        /// <summary>Gets the content of the mail.</summary>
+        /// <param name="mailId">The mail identifier.</param>
+        /// <returns></returns>
         public MailContent GetMailContent(string mailId)
         {
             return GetMailContentAsync(mailId).Result;
         }
 
+        /// <summary>Gets the email address asynchronous.</summary>
+        /// <returns></returns>
         public async Task<string> GetEmailAddressAsync()
         {
             return (await GetMailResponseAsync()).MailGetMail;
         }
 
 
+        /// <summary>Gets the seconds left asynchronous.</summary>
+        /// <returns></returns>
         public async Task<long> GetSecondsLeftAsync()
         {
             return (await GetMailResponseAsync()).MailLeftTime;
         }
 
 
+        /// <summary>Gets the emails count asynchronous.</summary>
+        /// <returns></returns>
         public async Task<int> GetEmailsCountAsync()
         {
             return (await GetMailResponseAsync()).MailList.Count;
         }
 
 
+        /// <summary>Gets the emails asynchronous.</summary>
+        /// <returns></returns>
         public async Task<List<MailContent>> GetEmailsAsync()
         {
             var response = await GetMailResponseAsync();
@@ -93,6 +121,8 @@ namespace _10MinuteMail.Net
             return mails.ToList();
         }
 
+        /// <summary>Gets the emails.</summary>
+        /// <value>The emails.</value>
         public List<MailContent> Emails => GetEmailsAsync().Result;
 
         public async Task<MailContent> GetLastEmailAsync()
@@ -101,42 +131,55 @@ namespace _10MinuteMail.Net
             return await GetMailContentAsync(response.MailList[0].MailId);
         }
 
+        /// <summary>Reset 10 minutes asynchronous.</summary>
+        /// <returns></returns>
         public async Task Reset10MinutesAsync()
         {
             await GetResponseString("/more.html");
         }
 
+        /// <summary>Reset 10 minutes.</summary>
         public void Reset10Minutes()
         {
             Reset10MinutesAsync().Wait();
         }
 
+        /// <summary>Reset 100 minutes asynchronous.</summary>
+        /// <returns></returns>
         public async Task Reset100MinutesAsync()
         {
             await GetResponseString("/more100.html");
         }
 
+        /// <summary>Reset 100 minutes.</summary>
         public void Reset100Minutes()
         {
             Reset100MinutesAsync().Wait();
         }
 
+        /// <summary>Generates the new email address asynchronous.</summary>
+        /// <returns></returns>
         public async Task<string> GenerateNewEmailAddressAsync()
         {
             await GetResponseString("/new.html");
             return await GetEmailAddressAsync();
         }
 
+        /// <summary>Generates the new email address.</summary>
+        /// <returns></returns>
         public string GenerateNewEmailAddress()
         {
             return GenerateNewEmailAddressAsync().Result;
         }
 
+        /// <summary>Recovers the expired email address asynchronous.</summary>
+        /// <returns></returns>
         public async Task RecoverExpiredEmailAddressAsync()
         {
             await GetResponseString("/recover.html");
         }
 
+        /// <summary>Recovers the expired email address.</summary>
         public void RecoverExpiredEmailAddress()
         {
             RecoverExpiredEmailAddressAsync().Wait();
--- README.md
+++ README.md
@@ -1,17 +1,9 @@
-# 10MinuteMail
-10MinuteMail API clients
+## 10MinuteMail.net API client 
+  [![Build status](https://ci.appveyor.com/api/projects/status/v4l997q4qyq88ily?svg=true)](https://ci.appveyor.com/project/unickq/10minutemail)
+  [![Test status](http://teststatusbadge.azurewebsites.net/api/status/unickq/10minutemail)](https://ci.appveyor.com/project/unickq/10minutemail)
+  [![NuGet](http://flauschig.ch/nubadge.php?id=10minutesMail.net)](https://www.nuget.org/packages/10minutesMail.net)
 
-## 10MinuteMail.net
-![10MinuteMail.net](https://10MinuteMail.net)
-
-[![Build status](https://ci.appveyor.com/api/projects/status/5nomj0qw25bo8gnv?svg=true)](https://ci.appveyor.com/project/unickq/allure-nunit)[![NuGet](http://flauschig.ch/nubadge.php?id=NUnit.Allure)](https://www.nuget.org/packages/NUnit.Allure)
-
-### Allure report:
-
-![Allure report](https://raw.githubusercontent.com/unickq/allure-nunit/master/AllureScreen.png)
-
-### Code example:
-
+#### Code example:
 ```cs
 public class Example
 {
@@ -37,8 +29,4 @@ public class Example
         await t.Reset100MinutesAsync(); //Reset email - add 100 minutes
     }
 }
-```  
-
-### 10minutesMail.net
-[![Build status](https://ci.appveyor.com/api/projects/status/v4l997q4qyq88ily?svg=true)](https://ci.appveyor.com/project/unickq/10minutemail)
-[![NuGet](http://flauschig.ch/nubadge.php?id=10minutesMail.net)](https://www.nuget.org/packages/10minutesMail.net)
+```
